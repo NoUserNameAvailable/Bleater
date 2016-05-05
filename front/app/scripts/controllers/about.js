1e95ac63f8af6c54467dde2f50cd5f66939e9a2e
Mathieu <mathieu.laplaige@gmail.com>
2016-05-05 16:27:26 +0200

    go

---
+++ front/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bleaterApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bleaterApp
+ */
+angular.module('bleaterApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ front/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bleaterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bleaterApp
+ */
+angular.module('bleaterApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
